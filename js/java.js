7713473973d9870d1d57b4468623aadb8ea6991c
esleik <98020809+esleik@users.noreply.github.com>
2023-10-24 10:19:53 +0200

    alejandro actualizacion

---
+++ js/java.js
@@ -0,0 +1,40 @@
+const juegos = [
+    {
+        titulo: "Pokemon Escarlata/Purpura DLC",
+        precio: "0",
+    },
+    {
+        titulo: "Grand Theft Auto V",
+        precio: "0",
+    },
+    {
+        titulo: "Spider-Man Miles Morales",
+        precio: "0",
+    },
+    {
+        titulo: "The Legend of Zelda: Tears of the Kingdom",
+        precio: "0",
+    },
+    {
+        titulo: "Atomic Heart",
+        precio: "0",
+    },
+    {
+        titulo: "Far Cry 6",
+        precio: "0",
+    },
+    {
+        titulo: "One Piece Odyssey",
+        precio: "0",
+    },
+    {
+        titulo: "Hallo Infinite",
+        precio: "0",
+    },
+    {
+        titulo: "Call Of Duty Modern Warfare 2",
+        precio: "0",
+    }
+]
+
+  

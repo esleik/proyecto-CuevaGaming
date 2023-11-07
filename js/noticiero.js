6fa57f532fdde6afe85667b6c1352a31ee1884ba
ismayax <ismailarfaoui14@gmail.com>
2023-11-07 11:24:27 +0100

    ismail

--- js/noticiero.js
+++ js/noticiero.js
@@ -1,40 +1,18 @@
-const juegos = [
-    {
-        titulo: "Pokemon Escarlata/Purpura DLC",
-        precio: "0",
-    },
-    {
-        titulo: "Grand Theft Auto V",
-        precio: "0",
-    },
-    {
-        titulo: "Spider-Man Miles Morales",
-        precio: "0",
-    },
-    {
-        titulo: "The Legend of Zelda: Tears of the Kingdom",
-        precio: "0",
-    },
-    {
-        titulo: "Atomic Heart",
-        precio: "0",
-    },
-    {
-        titulo: "Far Cry 6",
-        precio: "0",
-    },
-    {
-        titulo: "One Piece Odyssey",
-        precio: "0",
-    },
-    {
-        titulo: "Hallo Infinite",
-        precio: "0",
-    },
-    {
-        titulo: "Call Of Duty Modern Warfare 2",
-        precio: "0",
-    }
-]
+var modal = document.getElementById("modala");
+var img = document.getElementById("Img");
+var modalContent = document.querySelector(".modal-content");
+var span = document.getElementsByClassName("close")[0];
 
-  
+img.onclick = function () {
+    modal.style.display = "block";
+};
+
+span.onclick = function () {
+    modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};

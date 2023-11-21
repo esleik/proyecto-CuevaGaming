39ff936578b239aab8584cc94d8603587b661efb
codigomaxim <codigomaxim@gmail.com>
2023-11-21 13:03:11 +0100

    maxim22

--- js/suscribete.js
+++ js/suscribete.js
@@ -4,6 +4,7 @@ var movida = false; // Variable de estado
             var Flecha = document.getElementById('mostrarTexto');
             var Sobrenosotros = document.querySelector('.sobrenosotros');
             var Banderas = document.querySelector('.banderas');
+            var Participantes = document.querySelector('.participantes');
 
             var posicionFlecha = parseFloat(getComputedStyle(Flecha).top);
             var posicionSobrenosotros = parseFloat(getComputedStyle(Sobrenosotros).top);
@@ -11,14 +12,15 @@ var movida = false; // Variable de estado
 
             if (movida) {
                 // Si la flecha ya se movió, vuelve a su posición original
-                Flecha.style.top = '59%';
-                Sobrenosotros.style.top = '50%';
+                Flecha.style.top = '145%';
+                Sobrenosotros.style.top = '130%';
             } else {
                 // Si no se ha movido, se mueve hacia arriba
-                var posicionFlecha2 = posicionFlecha - 135;
-                var posicionSobrenosotros2 = posicionSobrenosotros - 135;
+                var posicionFlecha2 = posicionFlecha - 180;
+                var posicionSobrenosotros2 = posicionSobrenosotros - 180;
                 Flecha.style.top = posicionFlecha2 + 'px';
                 Sobrenosotros.style.top = posicionSobrenosotros2 + 'px';
+
             }
 
             // Cambiamos el estado
@@ -27,7 +29,19 @@ var movida = false; // Variable de estado
         
             if (movida) {
                 Banderas.style.display = 'block';
+                Participantes.style.display = 'block';
             } else {
                 Banderas.style.display = 'none';
+                Participantes.style.display = 'none';
             }
+        }
+
+        function redirigirAInstagram() {
+            window.open('https://www.instagram.com/cuevagaming', '_blank');
+        }
+        function redirigirATwitter() {
+            window.open('https://www.twitter.com/@cuevagaming', '_blank');
+        }
+        function redirigirATiktok() {
+            window.open('https://www.tiktok.com/@cuevagaming', '_blank');
         }

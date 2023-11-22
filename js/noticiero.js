dd85dc3129ff05009dff71c4f50adf2602df30fe
ismayax <ismailarfaoui14@gmail.com>
2023-11-22 10:31:01 +0100

    ismail

--- js/noticiero.js
+++ js/noticiero.js
@@ -1,18 +1,37 @@
-var modal = document.getElementById("modala");
-var img = document.getElementById("Img");
-var modalContent = document.querySelector(".modal-content");
-var span = document.getElementsByClassName("close")[0];
+function cerrarModal() {
+    document.getElementById('modalTrucos').style.display = 'none';
+}
 
-img.onclick = function () {
-    modal.style.display = "block";
-};
+// Esta función toma el índice del truco en el array como argumento
+function abrirModal(truco) {
+    document.getElementById('tituloTruco').innerText = truco.nombre;
+    document.getElementById('descripcionTruco').innerText = truco.truco1;  // Puedes mostrar más información según tu estructura de datos
+    document.getElementById('videoTruco').src = truco.video;
+    document.getElementById('modalTrucos').style.display = 'block';
+}
 
-span.onclick = function () {
-    modal.style.display = "none";
-};
+// Esta función toma el índice del truco en el array como argumento
+function actualizarEventListenersModal() {
+    const trucosElementos = document.querySelectorAll('.trucos-contenedor');
+    trucosElementos.forEach((element, index) => {
+        element.addEventListener('click', function () {
+            const truco = window.trucos[index];
+            abrirModal(truco);
+        });
+    });
+}
 
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-};
+// Llama a esta función cuando se cargue el JSON
+function cargarTrucos() {
+    // Puedes ajustar la ruta al archivo JSON según tu estructura de carpetas
+    fetch('ruta/al/archivo/trucos.json')
+        .then(response => response.json())
+        .then(data => {
+            window.trucos = data;
+            actualizarEventListenersModal();
+        })
+        .catch(error => console.error('Error cargando el archivo JSON', error));
+}
+
+// Llama a cargarTrucos cuando se cargue la página
+document.addEventListener('DOMContentLoaded', cargarTrucos);

4ec5a2c207e63132377f9aae15a03cb4088546e5
ismayax <ismailarfaoui14@gmail.com>
2023-11-23 12:16:12 +0100

    ismail

--- js/noticiero.js
+++ js/noticiero.js
@@ -6,15 +6,16 @@ var spans = document.querySelectorAll(".modala .close");
 imgs.forEach(function (img, index) {
     img.onclick = function () {
         modales[index].style.display = "block";
+
+        // Agregar evento de clic fuera de la ventana modal para cerrarla
+        window.onclick = function (event) {
+            if (event.target == modales[index]) {
+                modales[index].style.display = "none";
+            }
+        };
     };
 
     spans[index].onclick = function () {
         modales[index].style.display = "none";
-    };
-
-    window.onclick = function (event) {
-        if (event.target == modales[index]) {
-            modales[index].style.display = "none";
-        }
     };
 });
